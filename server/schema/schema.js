8e39c9222e34e66ae009e4151887c11b59c1388f
Kaleb <Kalebalebachew4@gmail.com>
2023-09-09 19:27:00 +0300

    schema update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -7,7 +7,8 @@ const {
     GraphQLID , 
     GraphQLString,
     GraphQLSchema,
-    GraphQLList 
+    GraphQLList ,
+    GraphQLNonNull
 } = require('graphql')
 
 const ClientType = new GraphQLObjectType({
@@ -66,8 +67,36 @@ const ProjectType = new GraphQLObjectType({
             }
         }
     });
+
+    //mutation
+    const mutation = new GraphQLObjectType({
+    
+        name: 'Mutation',
+        fields:{
+            addClient:{
+                type: ClientType,
+                args:{
+                    name:{type: GraphQLNonNull(GraphQLString)},
+                    email:{type: GraphQLNonNull(GraphQLString)},
+                    phone:{type: GraphQLNonNull(GraphQLString)},
+                },
+                resolve(parent, args){
+                    const client = new Client({
+                        name: args.name,
+                        email: args.email,
+                        phone: args.phone
+                    })
+                    return client.save()
+                }
+
+            }
+
+        }
+
+    })
     
 
 module.exports = new GraphQLSchema({
-    query: RootQuery
+    query: RootQuery,
+    mutation
 })
