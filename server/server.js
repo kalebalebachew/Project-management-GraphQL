5663dd3c6eb81919d9a5f825822f0893aad65e7c
Kaleb <Kalebalebachew4@gmail.com>
2023-10-20 00:01:23 +0300

    minor improvements

--- server/server.js
+++ server/server.js
@@ -1,14 +1,19 @@
 const express = require('express')
 require('dotenv').config()
+const cors = require('cors')
 const { graphqlHTTP} = require('express-graphql')
 const schema = require('./schema/schema')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
+
 const app = express()
 
 
+
 connectDB()
+
+app.use(cors())
 
 app.use(
     '/graphql',
